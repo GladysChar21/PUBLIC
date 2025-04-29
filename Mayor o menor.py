ceaa87a7f7bcc37a7b25d7af47fbfd3c1ed39322
GladysChar21 <gladys.charcopa0125@utc.edu.ec>
2025-04-29 13:44:20 -0500

    Subiendo

---
+++ Mayor o menor.py
@@ -0,0 +1,9 @@
+n1 = int(input("Ingrese el primer numero: "))
+n2 = int(input("Ingrese el segundo numero: "))
+
+if n1 > n2:
+    print(f"El numero {n1} es mayor a {n2}")
+elif n2 > n1:
+    print(f"El numero {n2} es mayot a {n1}")
+else:
+    print("Son iguales")
